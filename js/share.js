f8f21f56b6372e9749cf6e710c7283cd98f23330
Dayoung Kwon <da02@Dayoungui-MacBookPro.local>
2023-05-07 21:17:15 +0900

    modify link

--- js/share.js
+++ js/share.js
@@ -1,28 +1,30 @@
 function setShare(point) {
-	const shareImage = 'https://www.meboon.com/site/MBTI/image/shareImage.png';
-	const shareURL = 'https://www.meboon.com/site/MBTI/result.html?'+point;
-	Kakao.Link.sendDefault({
-		objectType: 'feed',
-		content: {
-			title: '미분양|뒷통수_얼얼 팩폭 테스트',
-			description: '#뒷통수_얼얼 #팩폭 #아닌척_자제좀 #이게너야ㅋㅋ #미분양 #mbti',
-			imageUrl: shareImage,
-			link: {
-				mobileWebUrl: shareURL,
-				webUrl: shareURL
-			},
-		},
-		buttons: [
-			{
-				title: '친구 결과 확인하기',
-				link: {
-					mobileWebUrl: shareURL,
-					webUrl: shareURL,
-				},
-			},
-		]
-	});
+  const shareImage =
+    "https://dayoungkwon.github.io/allzero.github.io/image/shareImage.png";
+  const shareURL =
+    "https://dayoungkwon.github.io/allzero.github.io//result.html?" + point;
+  Kakao.Link.sendDefault({
+    objectType: "feed",
+    content: {
+      title: "SW 진로설계",
+      description: "#개발자 #mbti #미랜",
+      imageUrl: shareImage,
+      link: {
+        mobileWebUrl: shareURL,
+        webUrl: shareURL,
+      },
+    },
+    buttons: [
+      {
+        title: "친구 결과 확인하기",
+        link: {
+          mobileWebUrl: shareURL,
+          webUrl: shareURL,
+        },
+      },
+    ],
+  });
 }
 function moveto() {
-	location.href='https://www.meboon.com/site/MBTI/MBTI.html';
+  location.href = "https://dayoungkwon.github.io/allzero.github.io/";
 }
